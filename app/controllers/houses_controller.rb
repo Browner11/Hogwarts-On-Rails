b111601ae883af33e49146b4822e348048eeaf34
Bryce <brycebrown91@hotmail.com>
2019-09-23 09:50:17 -0500

    Loading up house data in the house index view

--- app/controllers/houses_controller.rb
+++ app/controllers/houses_controller.rb
@@ -1,7 +1,11 @@
+# frozen_string_literal: true
+
 class HousesController < ApplicationController
   def index
+    @houses = House.all # This instance vaariable will be shared with the view
   end
+  # The associated view is auto-magically loaded with access to the @vars
+  # index action maps to: app/views/houses/index.html.erb
 
-  def show
-  end
+  def show; end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
-  resources :houses, only [:index, :show]
+  resources :houses, only: %i[index show]
   # GET /houses        => houses#index
   # GET /houses/:id    => houses#show
 
-
   root to: 'houses#index'
-  #GET /               => houses#index
+  # GET /               => houses#index
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
